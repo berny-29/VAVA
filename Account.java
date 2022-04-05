57712bc848e83c3ac1e7fb95cb41f1695b36744f
wolfenergi <denina.filo@gmail.com>
2022-04-05 10:00:06 +0200

    create Account

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    private int id ;
+    private String meno;
+    private String heslo;
+
+    public static void accountRegistration(){}
+    public static void pouzivatelLogin(String meno, String heslo){
+        //v tejto casti v podstate budeme tahat data z netu a skontrolujeme
+        // ci vsetko sedy, ak ano tak stiahneme ostatne data cca
+    }
+
+}
