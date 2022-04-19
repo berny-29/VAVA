13c8b03e2811099024b47ae18c7824058718e57c
ivangajdos <71788934+ivangajdos@users.noreply.github.com>
2022-04-19 10:09:12 +0200

    Add files via upload

---
+++ src/Model/Plan.java
@@ -0,0 +1,40 @@
+package src.Model;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+/**
+ * @author: Spol XD, akt(Filo,)
+ * describtion: ponechavam vasej predstavivosti
+*/
+public class Plan {
+    private OffsetDateTime start;       //Ten start a end sme ako mysleli v tom plane?
+    private OffsetDateTime end;
+    private ArrayList<Task> tasks;
+
+    Plan(){
+        this.tasks = new ArrayList<Task>();
+    }
+
+    //TODO este chyba geter a setter pre tasks, ale tam treba uvazovat ako budeme pristupovat k jednotlivim elementom?
+    // funkica nato je array.get(), ale ako ich budeme identifikovat?
+
+    public Task addTask(){
+        Task task = new Task();
+        this.tasks.add(task);
+        return task;
+    }
+    //Geters
+    public OffsetDateTime getStart() {
+        return start;
+    }
+    public OffsetDateTime getEnd() {
+        return end;
+    }
+    //Setters
+    public void setStart(OffsetDateTime start) {
+        this.start = start;
+    }
+    public void setEnd(OffsetDateTime end) {
+        this.end = end;
+    }
+}
