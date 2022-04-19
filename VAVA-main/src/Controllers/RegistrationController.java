a345d45f15f3b7f98aee5cb5f9ceb52d16e50854
ivangajdos <71788934+ivangajdos@users.noreply.github.com>
2022-04-19 10:21:06 +0200

    Add files via upload

---
+++ VAVA-main/src/Controllers/RegistrationController.java
@@ -0,0 +1,42 @@
+package src.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private CheckBox male;
+
+    @FXML
+    private CheckBox female;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private DatePicker birthDate;
+
+
+
+    private void register(){
+        String emailText = email.getText();
+        String
+
+    }
+}
---
+++ VAVA-main/src/Model/Account.java
@@ -0,0 +1,74 @@
+package src.Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author: Spol XD, akt(Filo,Gajdos,Mark)
+ * describtion: ponechavam vasej predstavivosti
+*/
+public class Account {
+    private int id ;
+    private String name, password, email;
+
+    public static void accountRegistration(String firstname,String lastname,String sex,String date, String password, String email) throws SQLException {
+        String regex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+
+        Matcher matcher = pattern.matcher(email);
+        if(!matcher.matches()){
+            //toto bude handlovat nejaky alert
+            System.out.println("Wrong email");
+        }else{
+            Connection conn = Database.getInstance().getConnection();
+
+            String find = "SELECT * FROM accounts where email = ?";
+            PreparedStatement pf = conn.prepareStatement(find);
+
+            pf.setString(1,email);
+            ResultSet rowf = pf.executeQuery();
+
+
+            if(!rowf.next()){
+                String insert = "INSERT INTO accounts (firstname,lastname,password,email,sex,date) VALUES(?,?,?,?,?,?)";
+
+                PreparedStatement pi = conn.prepareStatement(insert);
+                pi.setString(1,firstname);
+                pi.setString(2,lastname);
+                pi.setString(3,password);
+                pi.setString(4,email);
+                pi.setString(5,sex);
+                pi.setString(6,date);
+
+                int rowi = pi.executeUpdate();
+                System.out.println(rowi);
+            }else {
+                //toto bude handlovat nejaky alert
+                System.out.println("Email already registered");
+            }
+
+        }
+
+    }
+    public static void userLogin(String email, String password) throws SQLException {
+        Connection conn = Database.getInstance().getConnection();
+
+        String find = "SELECT * FROM accounts where email = ? and password = ?";
+        PreparedStatement pf = conn.prepareStatement(find);
+
+        pf.setString(1,email);
+        pf.setString(2,password);
+        ResultSet rowf = pf.executeQuery();
+        if(!rowf.next()){
+            System.out.println("Wrong email or password");
+        }else {
+            System.out.println("Welcome");
+        }
+    }
+}
