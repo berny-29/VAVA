afaa717f0fdb27404f163ba5b0ec625742baa28a
Samuel Bernat <samobernat@gmail.com>
2022-04-22 19:51:05 +0200

    Rozrobena Profile Page

--- VAVA-main/src/Controllers/ProfileController.java
+++ VAVA-main/src/Controllers/ProfileController.java
@@ -1,4 +1,23 @@
 package src.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+
 public class ProfileController extends Controller{
+
+    @FXML
+    private PieChart PieChart;
+
+    @FXML
+    private BarChart<?, ?> barChart;
+
+    @FXML
+    private Button languageSwitch;
+
+    @FXML
+    private Button logOutButton;
+
 }
+
