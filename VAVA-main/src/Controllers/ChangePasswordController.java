ecfa281c8b34ce6445bc4acb0c9fb26505f5b979
Samuel Bernat <samobernat@gmail.com>
2022-04-22 14:30:01 +0200

    ChangePassword hotovo

---
+++ VAVA-main/src/Controllers/ChangePasswordController.java
@@ -0,0 +1,49 @@
+package src.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ChangePasswordController {
+
+    @FXML
+    private Button BackButton;
+
+    @FXML
+    private Button changePwdBtn;
+
+    @FXML
+    private Text labe0;
+
+    @FXML
+    private Label labe1;
+
+    @FXML
+    private Label labe2;
+
+    @FXML
+    private Label labe3;
+
+    @FXML
+    private Label languageSwitch;
+
+    @FXML
+    private TextField myEmail;
+
+    @FXML
+    private PasswordField newPassword;
+
+    @FXML
+    private PasswordField newPasswordAgain;
+
+    @FXML
+    void changeEmail(ActionEvent event) {
+
+    }
+
+}
