7bc0ebe3d59aeb4ef5337bf5dbe6edb9779e49e3
Samuel Bernat <samobernat@gmail.com>
2022-04-24 12:32:23 +0200

    Admin GUI dokoncene

---
+++ VAVA-main/src/Controllers/AdminPageController.java
@@ -0,0 +1,49 @@
+package src.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+
+public class AdminPageController {
+
+    @FXML
+    private ToggleButton dayRepeatButton;
+
+    @FXML
+    private Button deleteTaskButton;
+
+    @FXML
+    private Label deleteTaskLabel;
+
+    @FXML
+    private Button languageButton;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private TextArea messageTextArea;
+
+    @FXML
+    private Button publishTaskButton;
+
+    @FXML
+    private TextField repeatInDayTextField;
+
+    @FXML
+    private Button sendMessageButton;
+
+    @FXML
+    private Label taskPublishedLabel;
+
+    @FXML
+    private TextArea userChatTextArea;
+
+    @FXML
+    private TextArea userLogsTextArea;
+
+}
